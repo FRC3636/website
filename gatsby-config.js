8123d1830567ff0f5e4dd45cf492a9f6778cba7b
Max Niederman <max@maxniederman.com>
2021-05-22 19:19:06 -0700

    fix: add temporary favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,7 +33,7 @@ module.exports = {
 				background_color: `#663399`,
 				theme_color: `#663399`,
 				display: `minimal-ui`,
-				icon: `src/images/logo.svg`,
+				icon: `src/images/logo.png`,
 			},
 		},
 		{
