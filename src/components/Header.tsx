08eecc63fe09422bdd2c73e5abee11426c50ea18
Max Niederman <maxniederman@gmail.com>
2022-03-29 16:55:27 -0700

    fix(nav): handroll popover

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "gatsby";
-import { Popover } from "@headlessui/react";
 import { Divide as Hamburger } from "hamburger-react";
 import Logo from "./Logo";
 import navigation from "../../content/navigation.yml";
@@ -8,6 +7,8 @@ import navigation from "../../content/navigation.yml";
 interface Props {}
 
 const Header: React.FC<Props> = (_props) => {
+	const [isOpen, setIsOpen] = React.useState(false);
+
 	return (
 		<nav className="flex justify-between">
 			<Link
@@ -20,27 +21,23 @@ const Header: React.FC<Props> = (_props) => {
 
 			<div className="colors-dark dark:colors-light rounded-bl">
 				{/* Dropdown Menu */}
-				<Popover className="flex flex-col sm:hidden">
-					{({ open }) => (
-						<>
-							<Popover.Button className="ml-auto rounded-bl colors-dark dark:colors-light">
-								<Hamburger toggled={open} size={25} />
-							</Popover.Button>
+				<div className="flex flex-col sm:hidden">
+					<span className="ml-auto rounded-bl colors-dark dark:colors-light" onClick={() => setIsOpen(!isOpen)}>
+						<Hamburger toggled={isOpen} size={25} />
+					</span>
 
-							<Popover.Panel className="relative flex flex-col items-end justify-between p-4 w-full">
-								{navigation.map(({ title, url }) => (
-									<Link
-										to={url}
-										className="nodefault text-lg"
-										activeClassName="underline"
-									>
-										<h1>{title}</h1>
-									</Link>
-								))}
-							</Popover.Panel>
-						</>
-					)}
-				</Popover>
+					{isOpen && <div className="relative flex flex-col items-end justify-between p-4 w-full">
+						{navigation.map(({ title, url }) => (
+							<Link
+								to={url}
+								className="nodefault text-lg"
+								activeClassName="underline"
+							>
+								<h1>{title}</h1>
+							</Link>
+						))}
+						</div>}
+				</div>
 
 				{/* Regular Menu */}
 				<div className="items-center space-x-2 px-4 hidden h-full sm:flex">
